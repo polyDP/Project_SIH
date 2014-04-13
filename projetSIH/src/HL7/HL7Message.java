43c106fe3244eb53a648dfe93ea90a8fe95316ff
yuya-Selena <is.chanet@gmail.com>
2014-04-13 13:54:45 +0200

    ajout package pour la création des messages HL7

---
+++ projetSIH/src/HL7/HL7Message.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package HL7;
+import SIH.*;
+/**
+ *
+ * @author indis_000
+ */
+public class HL7Message {
+    
+}
